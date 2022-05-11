25f59e1fb09fe3847c7ebed1d249dacfca596680
alaminhossainjibon <mdalamin7622@gmail.com>
2022-05-11 14:00:45 +0600

    navrar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
+import Contact from './Pages/Contact/Contact';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Review from './Pages/Review/Review';
+import Navbar from './Pages/Shared/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='appointment' element={<Appointment></Appointment>}></Route>
+        <Route path='review' element={<Review></Review>}></Route>
+        <Route path='contact' element={<Contact></Contact>}></Route>
+        <Route path='about' element={<About></About>}></Route>
+        <Route path='login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
