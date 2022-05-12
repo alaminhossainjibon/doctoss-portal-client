6121ad2ef80460c96c0ebc514588bb96191c65d5
alaminhossainjibon <mdalamin7622@gmail.com>
2022-05-12 20:31:00 +0600

    modal

---
+++ src/Pages/Appointment/Service.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Service = ({ service, setTreatment }) => {
+    const { name, slots } = service;
+    return (
+        <div className="card lg:max-w-lg bg-white shadow-xl mt-20">
+            <div className="card-body text-black">
+                <h2 className="card-title text-[#19D3AE]">{name}</h2>
+                <p>
+                    {
+                        slots.length > 0
+                            ? <span>{slots[0]}</span>
+                            : <span className='text-red-500'>Try another date.</span>
+                    }
+                </p>
+                <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
+                <div className="card-actions justify-center">
+                    <label
+                        for="booking-modal"
+                        disabled={slots.length === 0}
+                        onClick={() => setTreatment(service)}
+                        className="btn bg-[#19D3AE] text-white uppercase border-none">Book Appointment</label>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
